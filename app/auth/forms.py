fdde3a5e79ef244c17bd5cf291973b80513d0d65
ken <ken.mbira@student.moringaschool.com>
2021-09-25 16:53:37 +0300

    add a login page

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -13,7 +13,7 @@ class RegistrationForm(FlaskForm):
 
     username = StringField('Username',validators = [Required()])
     email = StringField('Email',validators=[Required(),Email()])
-    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password = PasswordField('Password',validators = [Required(), EqualTo('confirm_password',message = 'Passwords must match')])
     confirm_password = PasswordField('Confirm Passwords',validators = [Required()])
     submit = SubmitField('Create')
 
@@ -24,3 +24,13 @@ class RegistrationForm(FlaskForm):
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('That username is taken')
+
+class LoginForm(FlaskForm):
+    """This defines all the details of the user login
+
+    Args:
+        FlaskForm ([type]): [description]
+    """
+    email = StringField('Email',validators=[Required(),Email()])
+    password = PasswordField('Password',validators = [Required()])
+    submit = SubmitField('Log in')
--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,7 @@ from flask import render_template
 
 from . import auth
 from app.models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm,LoginForm
 
 @auth.route('/register')
 def register():
@@ -12,3 +12,10 @@ def register():
 
 
     return render_template('auth/register.html',form = form)
+
+@auth.route('/login')
+def login():
+    """This renders the content of the login page
+    """
+    form = LoginForm()
+    return render_template('auth/login.html',form = form)
