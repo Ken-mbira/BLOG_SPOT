90aa034b8b6966ec2120e8c76c8e8bcec98cd436
ken <ken.mbira@student.moringaschool.com>
2021-09-25 09:02:26 +0300

    add an authentication blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,8 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
 
-    @app.route('/')
-    def hello():
-        return "Hello World"
+    #registering blueprints
+    from app.auth import auth
+    app.register_blueprint(auth)
 
     return app
