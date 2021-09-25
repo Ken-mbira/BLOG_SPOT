80b40439bad38ca0f97fcd8b2ecb1ec83ce3303c
ken <ken.mbira@student.moringaschool.com>
2021-09-25 09:42:15 +0300

    add a navbar and footer file

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,7 +1,5 @@
 from flask import Blueprint
 
-main = Blueprint('main',__name__,url_prefix='/main')
+main = Blueprint('main',__name__)
 
-@main.route('/')
-def index():
-    return 'Hello main'
+from . import views
