99fbccef5bd755a66af2c9c8f0fdd4865583e8ed
ken <ken.mbira@student.moringaschool.com>
2021-09-26 15:42:48 +0300

    add to the create blog html to fill in data for the blog

--- app/blog/forms.py
+++ app/blog/forms.py
@@ -1,2 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField,StringField,SubmitField
+from wtforms import TextAreaField,StringField,SubmitField,RadioField
+from wtforms.validators import Required
+
+class BlogForm(FlaskForm):
+    """This defines all the data to be collected from a user for the blog
+
+    Args:
+        FlaskForm ([type]): [description]
+    """
+
+    title = StringField('Enter the title of the blog',validators=[Required()])
+    meta_title = StringField('Enter a brief summary of the blog',validators=[Required()])
+    blog = TextAreaField('Enter the blog itself')
+    category = RadioField('Enter the category in which your blog falls',validators=[Required()],choices=[('technology'),('finance'),('religion'),('fashion'),('food'),('music'),('lifestyle'),('sports'),('travel'),('politics'),('movie'),('gaming'),('cars')])
+    submit = SubmitField('Create Blog')
--- app/blog/views.py
+++ app/blog/views.py
@@ -1,5 +1,32 @@
-from . import blog
+from flask_login.utils import login_required
+from flask_login import current_user
+from flask import redirect,url_for,flash,render_template
 
-@blog.route('/')
-def view():
-    return 'Hello world'
+from app.models import Blog,Category,category_choice
+from . import blog
+from .forms import BlogForm
+from app import db
+
+@blog.route('/create_blog',methods = ['POST','GET'])
+@login_required
+def create_blog():
+    """This defines the creation of a new form
+    """
+    form = BlogForm()
+
+    if form.validate_on_submit():
+        blog = Blog(title = form.title.data,meta_title = form.title.data,body = form.blog.data,author_id = current_user.id)
+        db.session.add(blog)
+        category = Category.query.filter_by(category_name = form.category.data).first()
+        category.category.append(blog)
+
+        db.session.commit()
+
+        flash("Blog Created")
+        return redirect(url_for('blog.show_blogs'))
+
+    return render_template('blog/create_blog.html',form = form)
+
+@blog.route('/blogs')
+def show_blogs():
+    return render_template('blog/blogs.html')
