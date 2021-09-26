27582e377c23594c5bd2f9406c7d86927bf60654
ken <ken.mbira@student.moringaschool.com>
2021-09-26 13:31:38 +0300

    add a way to update ones profile

--- app/profile/forms.py
+++ app/profile/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField,StringField,SubmitField
+from wtforms.validators import Required
 
 class UpdateProfileForm(FlaskForm):
     """This defines the properties of the profile form
@@ -7,6 +8,6 @@ class UpdateProfileForm(FlaskForm):
     Args:
         FlaskForm ([type]): [description]
     """
-    bio = TextAreaField('Tell us a bit about yourself')
-    mobile = StringField('Enter your mobile number...')
+    bio = TextAreaField('Tell us a bit about yourself',validators=[Required()])
+    mobile = StringField('Enter your mobile number...',validators=[Required()])
     submit = SubmitField('Submit')
--- app/profile/views.py
+++ app/profile/views.py
@@ -1,5 +1,5 @@
 from flask_login.utils import login_required
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
 
 from . import profile
 from app.models import User
@@ -21,10 +21,49 @@ def profile_index(username):
 @profile.route('/<username>/update_pic',methods= ['POST'])
 @login_required
 def update_pic(username):
+    """This is responsible for updating the user profile pic
+
+    Args:
+        username ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
     user = User.query.filter_by(username = username).first()
+    if user is None:
+        abort(404)
+
     if 'photo' in request.files:
         filename = photos.save(request.files['photo'])
         path = f'photos/{filename}'
         user.profile_pic_path = path
         db.session.commit()
     return redirect(url_for('profile.profile_index',username = username))
+
+@profile.route('/<username>/update_profile',methods = ["POST","GET"])
+@login_required
+def update_profile(username):
+    """This is responsible for updating the user profile
+
+    Args:
+        username ([type]): [description]
+    """
+    user = User.query.filter_by(username = username).first()
+
+    if user is None:
+        abort(404)
+    else:
+    
+
+        form = UpdateProfileForm()
+
+        if form.validate_on_submit():
+            user.bio = form.bio.data
+            user.mobile = form.mobile.data
+
+            db.session.add(user)
+            db.session.commit()
+
+            return redirect(url_for('profile.profile_index',username = user.username))
+
+        return render_template('profile/update_profile.html',form = form)
